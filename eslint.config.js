9f1071e383a9ad3388f8498d9e5aca0792a4b373
David Nussbaum <dan4jy@virginia.edu>
2025-08-18 23:17:28 -0400

    "Eslint stuff"

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,1 @@
-export default [
-  {
-    files: ["**/*.js"],
-    rules: {
-      // Example: require semicolons
-      semi: ["error", "always"],
-      // quotes: ["error", "single"], // Remove or comment out this line
-    },
-  },
-];
+export default [];
