b539de39817985189a65c5685e66a3b2c1179202
David Nussbaum <dan4jy@virginia.edu>
2025-08-18 23:08:59 -0400

    "Initial Setup"

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'david-jackelyn-wedding',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'david-jackelyn-wedding',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
