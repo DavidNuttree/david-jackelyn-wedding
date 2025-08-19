f81abb516076bc6d3e2b2a6715bd66f6ed9e00dd
David Nussbaum <dan4jy@virginia.edu>
2025-08-18 23:11:39 -0400

    "Eslint addition"

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    files: ["**/*.js"],
+    rules: {
+      // Example: require semicolons
+      semi: ["error", "always"],
+      // Example: use single quotes
+      quotes: ["error", "single"],
+    },
+  },
+];
